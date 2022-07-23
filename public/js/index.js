940d72ed211945e808151613e01b2c365100e3dc
Naveen <naveenyadav7833@gmail.com>
2022-07-23 14:52:36 +0530

    SAVED FIRST TIME

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    document.querySelector("h1").classList.remove("visibility");
+}, 1500);
+setTimeout(function(){
+    document.querySelector(".head").classList.remove("visibility");
+}, 1900);
+setTimeout(function(){
+    document.querySelector(".display-6").classList.remove("visibility");
+}, 2300);
